58b26b3f9b63c2d67ab1d9b6224a9fe10b6780c7
POTLACHANDU <outerearthspace7345@gmail.com>
2024-05-16 15:01:12 +0530

    Message describing the changes

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from . models import post
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import post
 
 
 def home(request):
-    #posts = post.objects.order_by('date_posted')
-
     context = {
-        'posts':post.objects.all()
+        'posts': post.objects.all()
     }
-    #return render(request, 'blog/home.html', {'posts': posts})
-    post.objects.order_by('creation_time')
-    return render(request, "blog/home.html", context)
+    return render(request, 'blog/home.html', context)
+
 
 class PostListView(ListView):
     model = post
-    template_name = 'blog/home.html'
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    #ordering = ['-date_posted']
+    ordering = ['-date_post']
+
+
+class PostDetailView(DetailView):
+    model = post
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
