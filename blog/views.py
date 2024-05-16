d2a5a93565b7e8721fddf77e420892255947e2c7
POTLACHANDU <outerearthspace7345@gmail.com>
2024-05-16 15:16:06 +0530

    PostDetailView

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,8 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'posts'
 
 
 def about(request):
