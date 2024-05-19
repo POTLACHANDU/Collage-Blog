91063d0b79cc8c7593bc8b20d2091624f8aef77d
POTLACHANDU <outerearthspace7345@gmail.com>
2024-05-19 21:55:09 +0530

    Update and Delete

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from django.views.generic import (
     DeleteView
 )
 from .models import post
+from django.urls import reverse_lazy
 
 
 def home(request):
@@ -29,6 +30,50 @@ class PostDetailView(DetailView):
     template_name = 'blog/post_detail.html'
     context_object_name = 'posts'
 
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.pk})
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = post
+    fields = ['title', 'content']
+    template_name = 'blog/update_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    def get_object(self, queryset=None):
+        # Get the post object based on the pk provided in the URL
+        return post.objects.get(pk=self.kwargs['pk'])
+
+    def get_success_url(self):
+        # Redirect to the detail view of the updated post
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.pk})
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
